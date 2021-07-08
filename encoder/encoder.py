df987ad84ae28214184020d945175f418425ae9d
Sultan Sidhu <sultan.sidhu@mail.utoronto.ca>
2021-07-08 11:58:17 -0400

    added directory structure

---
+++ encoder/encoder.py
@@ -0,0 +1,7 @@
+"""
+Encoder file for ML Models for the Fledge Federated Learning Framework. 
+Currently, development work taking place only for encoding PyTorch models with protobuf, and sending
+them over a network using gRPC. 
+"""
+
+
