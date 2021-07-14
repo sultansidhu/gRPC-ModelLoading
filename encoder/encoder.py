dee42a4f63654e90d95f1aa19ecf478297e5e34f
Sultan Sidhu <sultan.sidhu@mail.utoronto.ca>
2021-07-14 12:19:44 -0400

    bug fix for ONNX encoding

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -4,6 +4,7 @@ Currently, development work taking place only for encoding PyTorch models with p
 them over a network using gRPC. 
 """
 # torch imports
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -23,41 +24,6 @@ class ProtoEncoder:
         self.loss = loss
         self.manager = ONNXManager()
 
-    def encode_layer(self, layer:nn.Module, layers:ModelLayers) -> Layer:
-        """
-        A function to encode individual layers of a PyTorch model
-
-        Args:
-            layer (nn.Module): The layer that is to be encoded using Protobuf
-
-        Returns:
-            Layer: Encoded layer type for the input layer
-        """
-        # creates a new layer to be added to collection
-        layer_proto = Layer()
-
-        # based on layer type, set the Layer instance's type and features 
-        if isinstance(layer, nn.Linear):
-            layer_proto.type.append(Layer.LayerType.LINEAR)
-            lin_layer = layers.LinearLayer()
-            lin_layer.inFeatures.append(layer.in_features)
-            lin_layer.outFeatures.append(layer.out_features)
-            layers.linearLayers.append(lin_layer)
-        elif isinstance(layer, nn.ReLU):
-            layer_proto.type.append(Layer.LayerType.RELU)
-            relu_layer = layers.ReLULayer()
-            relu_layer.inPlace.append(layer.inplace)
-            layers.reluLayers.append(relu_layer)
-        elif isinstance(layer, nn.LogSoftmax):
-            layer_proto.type.append(Layer.LayerType.LOGSOFTMAX)
-            logsoftmax_layer = layers.LogSoftmaxLayer()
-            logsoftmax_layer.dim.append(layer.dim)
-            layers.logsoftmaxLayers.append(logsoftmax_layer)
-        else:
-            print(f"Warning: Layer {layer} does not have a ProtoBuf Mapping. Program will exit.")
-            exit(1)
-        return layer_proto
-
     def encode_model_layers(self) -> bytes:
         """
         A function that encodes a given PyTorch model's layers using Protobuf.
@@ -66,7 +32,7 @@ class ProtoEncoder:
             bytes: An encoded byte-string which contains within it information regarding
             the layers of the encoded model. Encoded as ONNX.
         """
-        return self.manager.encode(self.model, (28*28, 28*28))
+        return self.manager.encode(self.model, torch.randn((28*28, 28*28)))
 
 
 
